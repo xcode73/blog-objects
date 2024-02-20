ba62197b2c75c50e75bb808afde67625bc7d98b9
xcode73 <xcode.uln@gmail.com>
2024-02-20 15:07:48 +0400

    update platform to macOS 12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "blog-objects",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "BlogObjects", targets: ["BlogObjects"]),
